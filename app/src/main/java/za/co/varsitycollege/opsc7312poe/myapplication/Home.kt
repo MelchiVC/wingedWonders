ff4b539ac08c4d99ea93ce35adfadb22b939dd21
yi <yifanstephen@gmail.com>
2023-10-01 14:48:55 +0200

    merged with tristans branch, fixed the error for vaild name 11, updated upload function.

--- app/src/main/java/za/co/varsitycollege/opsc7312poe/myapplication/Home.kt
+++ app/src/main/java/za/co/varsitycollege/opsc7312poe/myapplication/Home.kt
@@ -1,11 +1,28 @@
 package za.co.varsitycollege.opsc7312poe.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Home : AppCompatActivity() {
+    private lateinit var greetingTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        greetingTextView = findViewById(R.id.hello_user)
+
+        // Retrieve the logged-in user's email from UserDataManager
+        val loggedInUser = UserDataManager.getInstance().getLoggedInUser()
+        val fullName = loggedInUser?.full_name
+
+        // Set the TextView text to "Hello" + user's name
+        greetingTextView.text = "Hello ${fullName ?: "Guest"}"
+
+        greetingTextView.setOnClickListener{
+            val registrationIntent = Intent(this, Map::class.java)
+            startActivity(registrationIntent)
+        }
     }
 }
--- app/src/main/java/za/co/varsitycollege/opsc7312poe/myapplication/Sightings.kt
+++ app/src/main/java/za/co/varsitycollege/opsc7312poe/myapplication/Sightings.kt
@@ -1,11 +1,125 @@
 package za.co.varsitycollege.opsc7312poe.myapplication
-
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.storage.FirebaseStorage
+import za.co.varsitycollege.opsc7312poe.myapplication.databinding.ActivitySightingsBinding
+import java.io.ByteArrayOutputStream
 
 class Sightings : AppCompatActivity() {
+    private lateinit var binding: ActivitySightingsBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: DatabaseReference
+    private var imageUri: Uri? = null
+    private  var request_gallery=100
+    private  var request_carmera=200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sightings)
-    }
+        binding= ActivitySightingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//on click listener to open a dialog to let users to decdie using camera or storage to select image
+        binding.addPhotoButton.setOnClickListener{
+val builder = AlertDialog.Builder(this)
+           builder.setTitle("Upload image methods")
+           builder.setMessage("Choose a method to upload image")
+           builder.setPositiveButton("Gallery") { dialog: DialogInterface, which: Int ->
+               dialog.dismiss()
+               selectImage()
+           }
+           builder.setNegativeButton("Camera") { dialog: DialogInterface, which: Int ->
+               dialog.dismiss()
+               carmeraImage()
+           }
+val dialog:AlertDialog=builder.create()
+        dialog.show()
 }
+
+        binding.saveButton.setOnClickListener{
+uploadImage()
+        }
+
+
+    }
+
+    // Function to select image from local storage
+private fun selectImage() {
+    val selectIntent = Intent()
+    selectIntent.type="image/*"
+    startActivityForResult(selectIntent,request_gallery)
+}
+    // function to use carmera to take a photo
+    private fun carmeraImage(){
+        val cameraIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(cameraIntent.resolveActivity(packageManager) !=null) {
+            val permission =ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA)
+            if(permission !=PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),1 )
+            }
+            else{
+                startActivityForResult(cameraIntent,request_carmera)
+            }
+        }
+
+        }
+
+    // Function used after successfully pick image and set image uri
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == request_gallery && data != null) {
+             imageUri = data?.data!!
+            binding.birdImageView.setImageURI(imageUri)
+        }
+        else if (resultCode == RESULT_OK && requestCode == request_carmera && data != null){
+            val imageBitmap = data.extras?.get("data") as Bitmap
+            // Convert Bitmap to Uri
+            imageUri = getImageUriFromBitmap(imageBitmap)
+
+            binding.birdImageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private fun getImageUriFromBitmap(bitmap: Bitmap): Uri {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val path = MediaStore.Images.Media.insertImage(contentResolver, bitmap, "Title", null)
+        return Uri.parse(path)
+    }
+
+    //function to upload image to firebase
+    private fun uploadImage(){
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+        if (user != null) {
+            val uid = user.uid
+
+        if (imageUri != null) {
+            val storageRef = FirebaseStorage.getInstance().reference
+                .child(uid)
+                .child("bird_images")
+                .child("${System.currentTimeMillis()}.jpg")
+
+            storageRef.putFile(imageUri!!)
+                .addOnSuccessListener { taskSnapshot ->
+                    Toast.makeText(this, "Image uploaded successfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this, "Image upload failed: ${exception.message}", Toast.LENGTH_SHORT).show()
+                }}else{
+            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show() }
+        } else {
+            Toast.makeText(this, "User not authenticated", Toast.LENGTH_SHORT).show()
+        }}}
